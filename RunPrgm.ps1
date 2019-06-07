925393ec7f13f2557dea3d860c66089d472f865a
TheMohawkNinja <themohawkninja@aol.com>
2019-06-06 21:21:46 -0400

    Modified PID find functionality for Steam games
    PID find functionality for Steam games now uses a webcrawler to pull the game's title rather than relying on the user's command for the game.

--- RunPrgm.ps1
+++ RunPrgm.ps1
@@ -7,7 +7,6 @@ $ID=(query session | grep $user | grep -o [0-9])
 if($task -eq "start")
 {
     #Check if path is a steam gameid path or a true program path
-    echo "$dir \n"
     if($dir -eq "steam://rungameid")
     {
         psexec -nobanner -accepteula -i $ID -s cmd /s /c "start steam://rungameid/$prgm"
@@ -20,6 +19,81 @@ if($task -eq "start")
 }
 elseif($task -eq "kill")
 {
-    $wpid=cmd /s /c "tasklist -v | grep -i $name | grep -o -E '[0-9]{4,}'"
+    echo dir=$dir
+    #Check if path is a steam gameid path or a true program path
+    if($dir -eq "steam://rungameid")
+    {
+        echo Steam
+
+        #Basically everything from here to setting the $wpid variable is to allow the curl command to handle the fact that Steam is https
+        #C# class to create callback
+        $code = @"
+        public class SSLHandler
+        {
+            public static System.Net.Security.RemoteCertificateValidationCallback GetSSLHandler()
+            {
+
+                return new System.Net.Security.RemoteCertificateValidationCallback((sender, certificate, chain, policyErrors) => { return true; });
+            }
+
+        }
+"@
+
+        #compile the class
+        Add-Type -TypeDefinition $code
+
+        #disable checks using new class
+        [System.Net.ServicePointManager]::ServerCertificateValidationCallback = [SSLHandler]::GetSSLHandler()
+        [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+        #do the request
+        try
+        {
+            echo "Trying to parse name from Steam Store..."
+            echo "gameid=$prgm"
+            $name=curl -uri https://store.steampowered.com/app/$prgm | Select-Object -Expand Content | grep -o -P '(?<=<title>).*(?= on Steam</title>)'
+        }
+        catch
+        {
+            # do something
+        }
+        finally
+        {
+           #enable checks again
+           [System.Net.ServicePointManager]::ServerCertificateValidationCallback = $null
+        }
+        $wpid=cmd /s /c "tasklist -v | grep -i '$name' | grep -o -E '[0-9]{4,}'"
+        echo name=$name
+    }
+    else
+    {
+        $wpid=cmd /s /c "tasklist -v | grep -i '$prgm' | grep -o -E '[0-9]{4,}'"
+        echo prgm=$prgm
+    }
+    echo pid=$wpid
     kill -Force $wpid
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
